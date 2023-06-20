3cdc4297298bca15536958a05b0c6efd3ce39d06
Yuval Shalgi <yuvals@perfectomobile.com>
2023-06-20 11:38:13 +0300

    clean

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
-import Image from 'next/image'
+import Image from "next/image";
 //import styles from './page.scss'
-import {Game} from "@/app/components/Game";
-import  "./page.scss";
+import { Game } from "@/app/components/Game";
+import "./page.scss";
 export default function Home() {
   return (
     <main className={"main"}>
-
-        <div>
-          <h1>Minesweeper</h1>
-        </div>
-        <div className="game-wrapper">
-            <Game />
-        </div>
-
+      <div className="game-title">
+        <h1>Minesweeper</h1>
+      </div>
+      <div className="game-wrapper">
+        <Game />
+      </div>
     </main>
-  )
+  );
 }
