751ea55c6a9a7149d71aabe3dd265104412a8931
Yuval Shalgi <yuvals@perfectomobile.com>
2023-07-05 17:49:37 +0300

    foward headers

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
-// You might need to install axios using: npm install axios
 export default async (req, res) => {
   if (req.method === "POST") {
     try {
-      const response = await axios.post("http://54.167.14.4/chat", req.body);
+      const response = await axios.post("http://54.167.14.4/chat", req.body, {
+        headers: req.headers,
+      });
       res.status(200).json(response.data);
     } catch (error) {
       res.status(500).json({ error: error.toString() });
