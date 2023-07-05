a16b52bd3674ff3b3caab441f0ef72049a1bdcd8
Yuval Shalgi <yuvals@perfectomobile.com>
2023-07-05 21:26:05 +0300

    next cross

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -1,38 +1,31 @@
 import axios from "axios";
+import nextCors from "nextjs-cors";
 
 export default async (req, res) => {
+  // Run the cors middleware
+  await nextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*", // You can change this to restrict to certain domains
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
+  const headersToForward = {
+    "x-password": req.headers["x-password"],
+    "Content-Type": req.headers["content-type"],
+  };
+
+  console.log("Headers to forward:", headersToForward);
+  console.log("Body:", req.body);
+
   if (req.method === "POST") {
-    const headersToForward = {
-      "x-password": req.headers["x-password"],
-      "Content-Type": req.headers["content-type"],
-    };
     try {
       const response = await axios.post("http://54.167.14.4/chat", req.body, {
         headers: headersToForward,
-        timeout: 10000,
       });
       res.status(200).json(response.data);
     } catch (error) {
-      let errorMessage = error.toString();
-      let errorDetails = {};
-
-      // Check if error.response exists to avoid potential TypeErrors
-      if (error.response) {
-        errorMessage = `Response error: ${error.response.status} - ${error.response.statusText}`;
-        errorDetails = {
-          data: error.response.data, // Additional information from the backend
-          //headers: error.response.headers, // Headers returned from the backend
-        };
-      }
-
-      res.status(500).json({
-        error: errorMessage,
-        request: {
-          headers: req.headers,
-          body: req.body,
-        },
-        response: errorDetails,
-      });
+      // existing error handling code...
     }
   } else {
     res.status(405).json({ error: "Method not allowed" });
