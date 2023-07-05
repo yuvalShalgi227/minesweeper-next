56b954b3f8a9e1f02c03e2784cff6b8ca6145265
Yuval Shalgi <yuvals@perfectomobile.com>
2023-07-05 18:17:24 +0300

    witout next-connect

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -1,38 +1,35 @@
 import axios from "axios";
-import nextConnect from "next-connect";
 
-const handler = nextConnect();
-
-handler.use(express.json());
-
-handler.post(async (req, res) => {
-  try {
-    const response = await axios.post("http://54.167.14.4/chat", req.body, {
-      headers: req.headers,
-    });
-    res.status(200).json(response.data);
-  } catch (error) {
-    let errorMessage = error.toString();
-    let errorDetails = {};
-
-    // Check if error.response exists to avoid potential TypeErrors
-    if (error.response) {
-      errorMessage = `Response error: ${error.response.status} - ${error.response.statusText}`;
-      errorDetails = {
-        data: error.response.data, // Additional information from the backend
-        headers: error.response.headers, // Headers returned from the backend
-      };
-    }
-
-    res.status(500).json({
-      error: errorMessage,
-      request: {
+export default async (req, res) => {
+  if (req.method === "POST") {
+    try {
+      const response = await axios.post("http://54.167.14.4/chat", req.body, {
         headers: req.headers,
-        body: req.body,
-      },
-      response: errorDetails,
-    });
-  }
-});
+      });
+      res.status(200).json(response.data);
+    } catch (error) {
+      let errorMessage = error.toString();
+      let errorDetails = {};
 
-export default handler;
+      // Check if error.response exists to avoid potential TypeErrors
+      if (error.response) {
+        errorMessage = `Response error: ${error.response.status} - ${error.response.statusText}`;
+        errorDetails = {
+          data: error.response.data, // Additional information from the backend
+          headers: error.response.headers, // Headers returned from the backend
+        };
+      }
+
+      res.status(500).json({
+        error: errorMessage,
+        request: {
+          headers: req.headers,
+          body: req.body,
+        },
+        response: errorDetails,
+      });
+    }
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
+  }
+};
