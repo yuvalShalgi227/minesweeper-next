e3e855541cd1ade4e2ef4592700c44bc921ae848
Yuval Shalgi <yuvals@perfectomobile.com>
2023-07-05 18:12:56 +0300

    more error details

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -1,16 +1,38 @@
 import axios from "axios";
+import nextConnect from "next-connect";
 
-export default async (req, res) => {
-  if (req.method === "POST") {
-    try {
-      const response = await axios.post("http://54.167.14.4/chat", req.body, {
-        headers: req.headers,
-      });
-      res.status(200).json(response.data);
-    } catch (error) {
-      res.status(500).json({ error: error.toString() });
+const handler = nextConnect();
+
+handler.use(express.json());
+
+handler.post(async (req, res) => {
+  try {
+    const response = await axios.post("http://54.167.14.4/chat", req.body, {
+      headers: req.headers,
+    });
+    res.status(200).json(response.data);
+  } catch (error) {
+    let errorMessage = error.toString();
+    let errorDetails = {};
+
+    // Check if error.response exists to avoid potential TypeErrors
+    if (error.response) {
+      errorMessage = `Response error: ${error.response.status} - ${error.response.statusText}`;
+      errorDetails = {
+        data: error.response.data, // Additional information from the backend
+        headers: error.response.headers, // Headers returned from the backend
+      };
     }
-  } else {
-    res.status(405).json({ error: "Method not allowed" });
+
+    res.status(500).json({
+      error: errorMessage,
+      request: {
+        headers: req.headers,
+        body: req.body,
+      },
+      response: errorDetails,
+    });
   }
-};
+});
+
+export default handler;
