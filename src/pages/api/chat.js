2920fcfcbffdcf21c8355abd39e1af039d993844
Yuval Shalgi <yuvals@perfectomobile.com>
2023-07-05 23:23:05 +0300

    instead of next core

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -1,22 +1,18 @@
 import axios from "axios";
-import nextCors from "nextjs-cors";
 
 export default async (req, res) => {
-  // Run the cors middleware
-  await nextCors(req, res, {
-    // Options
-    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-    origin: "*", // You can change this to restrict to certain domains
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  });
+  // Manually set the CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, x-password");
 
   const headersToForward = {
     "x-password": req.headers["x-password"],
     "Content-Type": req.headers["content-type"],
   };
-
-  console.log("Headers to forward:", headersToForward);
-  console.log("Body:", req.body);
 
   if (req.method === "POST") {
     try {
